44364727c28a6d174cadd61ce7119c286f41e8dc
Katherine Schinkel <kms6bn@virginia.edu>
2015-11-18 23:44:50 -0500

    added Paragraph Location and fixed Youtube and Facebook reading

--- TermsOfService.py
+++ TermsOfService.py
@@ -11,21 +11,20 @@ import urllib
 
 #locations of terms of service
 urls = ["http://policies.yahoo.com/us/en/yahoo/terms/utos/index.htm", "https://www.google.com/intl/en/policies/terms/",
-        "https://www.facebook.com/legal/terms",
         "https://help.github.com/articles/github-terms-of-service/",
-        "https://wikimediafoundation.org/wiki/Terms_of_Use", "http://www.amazon.com/gp/help/customer/display.html/?ie=UTF8&nodeId=508088",
-        "https://www.youtube.com/t/terms"]
+        "https://wikimediafoundation.org/wiki/Terms_of_Use", "http://www.amazon.com/gp/help/customer/display.html/?ie=UTF8&nodeId=508088"]
 
 urls2 = ["https://help.instagram.com/478745558852511"]
 
-urls3 = ["https://www.netflix.com/TermsOfUse"]
+urls3 = ["https://www.netflix.com/TermsOfUse", "https://www.facebook.com/legal/terms",
+         "https://www.youtube.com/t/terms"]
 
 #corresponding companies          
-companies = ["Yahoo", "Google", "Facebook", "GitHub", "Wikipedia", "Amazon", "Youtube"]
+companies = ["Yahoo", "Google", "GitHub", "Wikipedia", "Amazon"]
 
 companies2 = ["Instagram"]
 
-companies3 = ["Netflix"]
+companies3 = ["Netflix", "Facebook", "Youtube"]
 
 #create empty data frame
 df = pd.DataFrame()
@@ -42,8 +41,11 @@ for url in urls:
     for paragraph in para:
         paraText = paragraph.get_text()
         paraText = paraText.strip()
-        rowValue = pd.Series([companies[index], paraText, len(paraText), int(paraText.count(' '))])
-        df = df.append(rowValue, ignore_index=True)
+        if (paraText != ""):
+            capsCount = sum(1 for x in paraText if x.isupper())
+            rowValue = pd.Series([companies[index], paraText, len(paraText), 
+                                  paraText.count(' '), capsCount])
+            df = df.append(rowValue, ignore_index=True)
 
 for url in urls2:
     index = urls2.index(url)
@@ -56,8 +58,11 @@ for url in urls2:
         para = str(paragraph.nextSibling.nextSibling)
         paraText = BeautifulSoup(para).get_text()
         paraText = paraText.strip()
-        rowValue = pd.Series([companies2[index], paraText, len(paraText), paraText.count(' ')])
-        df = df.append(rowValue, ignore_index=True)
+        if (paraText != ""):
+            capsCount = sum(1 for x in paraText if x.isupper())
+            rowValue = pd.Series([companies2[index], paraText, len(paraText), 
+                                  paraText.count(' '), capsCount])
+            df = df.append(rowValue, ignore_index=True)
         
 for url in urls3:
     index = urls3.index(url)
@@ -69,17 +74,45 @@ for url in urls3:
     for paragraph in para:
         paraText = paragraph.get_text()
         paraText = paraText.strip()
-        rowValue = pd.Series([companies3[index], paraText, len(paraText), paraText.count(' ')])
-        df = df.append(rowValue, ignore_index=True)
+        if (paraText != ""):
+            capsCount = sum(1 for x in paraText if x.isupper())
+            rowValue = pd.Series([companies3[index], paraText, len(paraText), 
+                                  paraText.count(' '), capsCount])
+            df = df.append(rowValue, ignore_index=True)
 
 #rename columns
-df.columns = ["Company", "ParagraphText", "ParagraphLength", "SpacesCount"]
+df.columns = ["Company", "ParagraphText", "ParagraphLength", "SpacesCount", "CapsCount"]
 
-#remove blank rows
+#calculate count of capital letters to length of paragraph value
+df["CapToLegthRatio"] = df["CapsCount"] / df["ParagraphLength"]
 
-#calculate porportion of where a paragraph is located within the terms of service
-#im thinking you would just use the order in the dataframe (i.e. if its the first
-#paragraph related to Facebook, then the value would be 1/(nrow(Facebook))) or something
+#calculate location of each paragraph in the terms of service
+#first calculate total paragraphs by company
+totalParaByCompany = pd.DataFrame(df.groupby(by=df["Company"]).size())
+
+#get the index of the first occurrence of each company
+companies = companies + companies2 + companies3 #put companies all in one vector
+firstIndex = []
+for company in companies:
+    firstIndex.append(list(df["Company"]).index(company))
+
+#mark each paragraph with its order in the terms
+df["ParaLocation"] = None
+for x in range(0, df.shape[0]):
+    if x == 0:
+        df["ParaLocation"][x] = 1
+
+    elif df["Company"][x] == df["Company"][x-1]:
+        df["ParaLocation"][x] = df["ParaLocation"][x-1] + 1
+        
+    else:
+        df["ParaLocation"][x] = 1
+
+#calculate order/total paragraphs
+for x in range(0, df.shape[0]):
+    company = df["Company"][x]
+    companyTotalPara = int(str(totalParaByCompany.loc[company]).split()[1]) #grabs total paragraphs from groupby results
+    df["ParaLocation"][x] = df["ParaLocation"][x] / companyTotalPara
 
 #print to csv
 #df.to_csv("TermsOfService.csv", index=False)
