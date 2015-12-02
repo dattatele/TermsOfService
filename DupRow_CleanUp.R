96ad88401dc1ba00d334fbd59ae8e3da076c7551
Hope McIntyre <hope.e.mcintyre@gmail.com>
2015-12-01 21:53:30 -0500

    Updated to Remove Documents that will become empty after stop word removal

--- DupRow_CleanUp.R
+++ DupRow_CleanUp.R
@@ -7,7 +7,13 @@ colnames(AG)
 # Row Index to be Deleted from Data Frame
 toDelete <- c(608,614,615,618,622,629,639,643,649,654,661,681,687,699,710,720,726,730,732,735,740,744,748,758)
 View(AG$ParagraphText[toDelete])
-AG <- AG[-toDelete,] 
+
+# Removing Empty Document Rows
+empty <- c(780,846,852)
+View(AG[empty,])
+
+# Remove both Duplicated Text and Empty Documents (After Stop Word Removal)
+AG <- AG[-c(toDelete,empty),] 
 
 # Delete Wikipedia
 AG <- AG[-which(AG$Company == "Wikipedia"),]
